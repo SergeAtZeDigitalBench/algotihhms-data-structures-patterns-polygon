dd51c724cbfb251e5f1ae5d3f54dcde1ec26b140
Serge Basangovs <sergeb@thedigitalbench.co.uk>
2023-06-29 14:17:46 +0100

    feat: basic express usage

--- src/express101/expressServer.js
+++ src/express101/expressServer.js
@@ -1,1 +1,12 @@
-console.log("express");
+const express = require("express");
+
+const PORT = 3000;
+const app = express();
+
+app.all("*", (req, res) => {
+  res.status(200).send("<h1>This is the homepage</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}`);
+});
