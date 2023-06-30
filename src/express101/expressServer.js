129850b16dba29e9407fc812819dcc308a383333
Serge Basangovs <sergeb@thedigitalbench.co.uk>
2023-06-30 07:50:38 +0100

    feat: express sstatic files

--- src/express101/expressServer.js
+++ src/express101/expressServer.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = 3000;
 const app = express();
 
-app.all("*", (req, res) => {
-  res.status(200).send("<h1>This is the homepage</h1>");
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.get("/", (req, res, next) => {
+  res.status(200);
+  fs.createReadStream(
+    path.resolve(__dirname, "templates", "homepage.html")
+  ).pipe(res);
+});
+
+app.all("*", (req, res, next) => {
+  res.status(404);
+  fs.createReadStream(path.resolve(__dirname, "templates", "404.html")).pipe(
+    res
+  );
 });
 
 app.listen(PORT, () => {
